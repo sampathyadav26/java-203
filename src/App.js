62a728bc915182b0a0ec223ba6f8fa067f457bab
Jaya Avinash Padala <jp22088@corp.ojas-it.com>
2022-09-06 09:51:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Formdata from './Components/Formdata';
+
+function App() {
+  return (
+    <div className="App">
+       <Formdata />
+    </div>
+  );
+}
+
+export default App;
