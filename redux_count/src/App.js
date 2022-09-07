676bcf70079fc979cd8d9a8061db06b42094844c
Manmadharao Maruvada <mm22042@corp.ojas-it.com>
2022-09-07 16:22:14 +0530

    react applications

---
+++ redux_count/src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from "react";
+import {connect} from 'react-redux';
+export class App extends Component {
+  increment = () => {
+    //increment action
+    this.props.dispatch({
+      type: "INCREMENT",
+    });
+  };
+
+  decrement = () => {
+    //Descrement action
+    this.props.dispatch({
+      type: "DECREMENT",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <center>
+          <h1 className="mt-5 display-1">{this.props.count}</h1>
+          <button onClick={this.increment} className="btn btn-success mr-5">
+            Increment
+          </button>
+          <span style={{ padding: 5 }}></span>
+          <button onClick={this.decrement} className="btn btn-danger">
+            Decrement
+          </button>
+        </center>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  //to access state mapState through access any component
+  return {
+    count: state.count,
+  };
+};
+
+export default connect(mapStateToProps)(App);
