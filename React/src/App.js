57f7951188fa01cfbd25b98a39d88a502a4b5f99
unknown <vovaldas.bhavyasri@gmail.com>
2022-09-07 16:30:24 +0530

    sdfhjh

---
+++ React/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import UsersList from './components/UsersList';
+function App() {
+  return (
+    <div className="App">
+      <UsersList />
+    </div>
+  );
+}
+
+export default App;
